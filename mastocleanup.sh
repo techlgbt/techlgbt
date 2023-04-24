739c537aca01d31585cd6b870d83bf726126102e
david wolfpaw <david@tech.lgbt>
2023-04-24 11:35:53 +0000

    updates mastocleanup tool

--- mastocleanup.sh
+++ mastocleanup.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+
+# print commands as they're executed, exit on error
+set -ex
+
 #export PATH="/home/mastodon/.rbenv/bin:$PATH"
 #eval "$(rbenv init -)"
-#export RAILS_ENV=production
+export RAILS_ENV=production
 
 #/home/mastodon/live/bin/tootctl statuses remove --days=3
 /home/mastodon/live/bin/tootctl media remove --days 30 --concurrency 100
@@ -9,6 +13,5 @@
 /home/mastodon/live/bin/tootctl media remove --prune-profiles -c 25
 #/home/mastodon/live/bin/tootctl preview_cards remove --days=180
 /home/mastodon/live/bin/tootctl cache clear
-
 
 echo `date +"%Y-%m-%d %T"` orphaned statuses and cached media removed
