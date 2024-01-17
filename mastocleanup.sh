ca4be06cd1338152aa46a48559bdf55744d31bfb
Jonathan de Jong <jonathandejong02@gmail.com>
2024-01-17 12:09:07 +0100

    fix cleanup script

--- mastocleanup.sh
+++ mastocleanup.sh
@@ -6,12 +6,15 @@ set -ex
 #export PATH="/home/mastodon/.rbenv/bin:$PATH"
 #eval "$(rbenv init -)"
 export RAILS_ENV=production
+export DB_NAME=cleanup
 
-#/home/mastodon/live/bin/tootctl statuses remove --days=240
-/home/mastodon/live/bin/tootctl media remove --days 30 --concurrency 100
-/home/mastodon/live/bin/tootctl media remove-orphans
-/home/mastodon/live/bin/tootctl media remove --prune-profiles -c 25
-#/home/mastodon/live/bin/tootctl preview_cards remove --days=180
-/home/mastodon/live/bin/tootctl cache clear
+pushd /home/mastodon/live
+
+#bundle exec bin/tootctl statuses remove --days=240
+bundle exec bin/tootctl media remove --days 30 --concurrency 100
+bundle exec bin/tootctl media remove-orphans
+bundle exec bin/tootctl media remove --prune-profiles -c 25
+#bundle exec bin/tootctl preview_cards remove --days=180
+bundle exec bin/tootctl cache clear
 
 echo `date +"%Y-%m-%d %T"` orphaned statuses and cached media removed
