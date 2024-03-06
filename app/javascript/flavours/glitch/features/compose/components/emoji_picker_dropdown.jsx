5f5274c3ea5af4592f30d69f8eac3e2f13ea3de3
Jonathan de Jong <jonathandejong02@gmail.com>
2024-03-06 18:21:27 +0100

    increase max results in emoji searcher fix emoji-mart version

--- app/javascript/flavours/glitch/features/compose/components/emoji_picker_dropdown.jsx
+++ app/javascript/flavours/glitch/features/compose/components/emoji_picker_dropdown.jsx
@@ -276,6 +276,7 @@ class EmojiPickerMenuImpl extends PureComponent {
         <EmojiPicker
           perLine={8}
           emojiSize={22}
+          maxResults={250}
           custom={buildCustomEmojis(custom_emojis)}
           color=''
           emoji=''
