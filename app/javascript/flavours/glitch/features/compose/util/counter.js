f0c60ebde216adc31bf204c663f460cd04372810
Jonathan de Jong <jonathandejong02@gmail.com>
2024-05-30 15:35:11 +0200

    fix wrong user counts on web

--- app/javascript/flavours/glitch/features/compose/util/counter.js
+++ app/javascript/flavours/glitch/features/compose/util/counter.js
@@ -5,5 +5,5 @@ const urlPlaceholder = '$2xxxxxxxxxxxxxxxxxxxxxxx';
 export function countableText(inputText) {
   return inputText
     .replace(urlRegex, urlPlaceholder)
-    .replace(/(^|[^/\w])@((\p{L}\p{M}*|[0-9_.-])+@[a-z0-9.-]+[a-z0-9]+)/igu, '$1@$3');
+    .replace(/(^|[^/\w])@(((?:\p{L}\p{M}*|[0-9_.-])+)@[a-z0-9.-]+[a-z0-9]+)/igu, '$1@$3');
 }
